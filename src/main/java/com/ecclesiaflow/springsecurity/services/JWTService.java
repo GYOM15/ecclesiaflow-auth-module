589697c11cfd2f3cbb93b8487ad71f6197a7544f
GYOM15 <millimounou.guy_olivier_yanouba@courrier.uqam.ca>
2025-06-17 10:47:08 -0400

    Create services package, Add JWTService interface

---
+++ src/main/java/com/ecclesiaflow/springsecurity/services/JWTService.java
@@ -0,0 +1,9 @@
+package com.ecclesiaflow.springsecurity.services;
+
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Map;
+
+
+public interface JWTService {
+    
+}
