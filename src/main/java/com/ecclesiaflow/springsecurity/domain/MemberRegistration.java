df846c82af1adb7fae799f742efc17d6aec16250
GYOM15 <millimounou.guy_olivier_yanouba@courrier.uqam.ca>
2025-07-25 22:05:41 -0400

    Add MemberRegistration Domain model

---
+++ src/main/java/com/ecclesiaflow/springsecurity/domain/MemberRegistration.java
@@ -0,0 +1,20 @@
+package com.ecclesiaflow.springsecurity.domain;
+
+public class MemberRegistration {
+    private final String firstName;
+    private final String lastName;
+    private final String email;
+    private final String password;
+
+    public MemberRegistration(String firstName, String lastName, String email, String password) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public String getFirstName() { return firstName; }
+    public String getLastName() { return lastName; }
+    public String getEmail() { return email; }
+    public String getPassword() { return password; }
+}
