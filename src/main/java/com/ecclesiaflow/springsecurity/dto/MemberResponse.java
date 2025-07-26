9cceb92401367847ce54d1bcaa699f21f4b877d7
GYOM15 <millimounou.guy_olivier_yanouba@courrier.uqam.ca>
2025-07-25 23:27:11 -0400

    Update MemberResponse to customize client response

--- src/main/java/com/ecclesiaflow/springsecurity/dto/MemberResponse.java
+++ src/main/java/com/ecclesiaflow/springsecurity/dto/MemberResponse.java
@@ -1,17 +1,26 @@
 package com.ecclesiaflow.springsecurity.dto;
 
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+
+import java.util.Collection;
 
 @Data
 @Builder
-@NoArgsConstructor
-@AllArgsConstructor
 public class MemberResponse {
     private String message;
     private String email;
+    private String firstName;
+    private String lastName;
+    private String password;
+    private String role;
+    private boolean accountNonLocked;
+    private boolean enabled;
     private String token;
+    private Collection<? extends GrantedAuthority> authorities;
+    private String username;
+    private boolean accountNonExpired;
+    private boolean credentialsNonExpired;
 }
 
