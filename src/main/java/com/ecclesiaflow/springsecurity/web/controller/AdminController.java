5d26c95ee90f492feeeaac560c7cfeaab87a6d22
GYOM15 <millimounou.guy_olivier_yanouba@courrier.uqam.ca>
2025-08-09 04:19:46 -0400

    Fix(admin): Update API path to ecclesiaflow

--- src/main/java/com/ecclesiaflow/springsecurity/web/controller/AdminController.java
+++ src/main/java/com/ecclesiaflow/springsecurity/web/controller/AdminController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/adminMembers")
+@RequestMapping("/ecclesiaflow/adminMembers")
 @RequiredArgsConstructor
 public class AdminController {
 
