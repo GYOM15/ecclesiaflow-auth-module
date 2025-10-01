57c7749ced94423b76666d04a2ee0963a3714cb1
GYOM15 <millimounou.guy_olivier_yanouba@courrier.uqam.ca>
2025-10-01 13:37:31 -0400

    Comment admin creation logic

--- src/main/java/com/ecclesiaflow/springsecurity/SpringsecurityApplication.java
+++ src/main/java/com/ecclesiaflow/springsecurity/SpringsecurityApplication.java
@@ -16,17 +16,17 @@ import java.util.UUID;
 @SpringBootApplication
 public class SpringsecurityApplication implements CommandLineRunner {
 
-	@Autowired
-	private MemberRepository memberRepository;
-
-	@Autowired
-	private PasswordEncoderUtil passwordEncoderUtil;
-
-	@Value("${admin.email}")
-	private String adminEmail;
-
-	@Value("${admin.password}")
-	private String adminPassword;
+//	@Autowired
+//	private MemberRepository memberRepository;
+//
+//	@Autowired
+//	private PasswordEncoderUtil passwordEncoderUtil;
+//
+//	@Value("${admin.email}")
+//	private String adminEmail;
+//
+//	@Value("${admin.password}")
+//	private String adminPassword;
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringsecurityApplication.class, args);
@@ -35,14 +35,14 @@ public class SpringsecurityApplication implements CommandLineRunner {
 	@Override
 	@Transactional
 	public void run(String... args) {
-		Member adminAccount = memberRepository.findByRole(Role.ADMIN);
-		if (adminAccount == null) {
-			Member member = Member.builder().
-					email(adminEmail).
-					password(passwordEncoderUtil.encode(adminPassword)).
-					role(Role.ADMIN).
-					build();
-			memberRepository.save(member);
-		}
+//		Member adminAccount = memberRepository.findByRole(Role.ADMIN);
+//		if (adminAccount == null) {
+//			Member member = Member.builder().
+//					email(adminEmail).
+//					password(passwordEncoderUtil.encode(adminPassword)).
+//					role(Role.ADMIN).
+//					build();
+//			memberRepository.save(member);
+//		}
 	}
 }
